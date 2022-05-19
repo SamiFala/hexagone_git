2bd2bd72e215de5abdd70e67a78f605ba3cbe1e6
jacie__ <nana.mboussi@gmail.com>
2022-05-19 16:06:14 +0200

    Naviation créée

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,6 +1,24 @@
 package com.example.myapplication
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.HomeLayoutBinding
+import com.example.myapplication.databinding.SplashLayoutBinding
 
 class HomeFragment : Fragment () {
+
+        private lateinit var binding: HomeLayoutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = HomeLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
