bc356f5d638edaa455776e6141de7386b9ec2450
FC86foot <elowan.engel@ecole-hexagone.com>
2022-05-19 16:49:45 +0200

    %s modification

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.navArgs
+import com.example.myapplication.databinding.HomeLayoutBinding
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
