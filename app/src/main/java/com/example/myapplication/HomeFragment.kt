98a02f97de24a3dc37c0a25fd0b0f7c3bea53e0c
LAPTOP-1ACG7QSV\Nina-Rachel <nina352009@hotmail.fr>
2022-05-19 16:08:57 +0200

    commit android studio 3

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,6 +1,24 @@
 package com.example.myapplication
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.HomeLayoutBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: HomeLayoutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = HomeLayoutBinding.inflate(inflater,container, false)
+        return binding.root
+        //return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
