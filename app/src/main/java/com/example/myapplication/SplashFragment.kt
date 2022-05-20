4c75e3a8097e4f6d5cbd3e5a28d537966b5dc537
LAPTOP-1ACG7QSV\Nina-Rachel <nina352009@hotmail.fr>
2022-05-20 10:24:02 +0200

    commit android studio 6

--- app/src/main/java/com/example/myapplication/SplashFragment.kt
+++ app/src/main/java/com/example/myapplication/SplashFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.SplashLayoutBinding
@@ -24,9 +25,13 @@ class SplashFragment : Fragment() {
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
             super.onViewCreated(view, savedInstanceState)
 
-
             binding.mbNav.setOnClickListener {
-                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment("Rachel"))
+                val name = binding.etName.text.toString()
+                if (!name.isNullOrBlank()) {
+                    findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment(name))
+                } else {
+                    Toast.makeText(requireContext(), " veuillez entrer votre nom", Toast.LENGTH_LONG).show()
+                }
             }
         }
 
