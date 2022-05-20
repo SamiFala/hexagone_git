3eaf2d8fafee58801932d85642654841f3975ec4
CHEBBI <Emna2605@gmail.com>
2022-05-20 15:18:17 +0200

    Création Bouton Reset!

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -49,7 +50,18 @@ class HomeFragment : Fragment() {
 
         }
 
+        ViewModel.error.observe(viewLifecycleOwner, Observer {
+            if (it){
+                Toast.makeText(requireContext(), "Une erreur est survenue", Toast.LENGTH_LONG).show()
+                //ViewModel.initError()
+            }
+        })
 
-        binding.tvTitle.text = getString(R.string.bonjour_s_et_bienvenue_sur_l_app, navArgs.name, "10")
-    }
+        binding.mbReset.setOnClickListener {
+                    ViewModel.reset()
+                }
+
+        //binding.tvTitle.text =
+                    getString(R.string.bonjour_s_et_bienvenue_sur_l_app, navArgs.name, "10")
+            }
 }
--- app/src/main/java/com/example/myapplication/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/MainViewModel.kt
@@ -7,19 +7,29 @@ import java.lang.invoke.MutableCallSite
 class MainViewModel : ViewModel() {
 
     var count : MutableLiveData<Int> = MutableLiveData(0)
-
+    var error : MutableLiveData<Boolean> = MutableLiveData(false)
     fun plus() {
         count.value = count.value?.plus(1)
     }
 
     fun minus() {
-        count.value = count.value?.minus(1)
+        count.value?.let {
+            if (it >= 1) {
+                count.value = count.value?.minus(1)
+            }
+            else{
+                error.value = true
+            }
+        }
         }
 
 
+
+    fun reset() {
+        count.value = 0
+    }
     }
 
 
 
 
-}
