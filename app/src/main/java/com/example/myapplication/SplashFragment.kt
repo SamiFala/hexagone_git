ded1aa9a3226b70602939ab1affcc982b17f4a62
CHEBBI <Emna2605@gmail.com>
2022-05-20 10:23:32 +0200

    affichage d'un toast si le champ votre nom est vide!

--- app/src/main/java/com/example/myapplication/SplashFragment.kt
+++ app/src/main/java/com/example/myapplication/SplashFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.SplashLayoutBinding
@@ -26,9 +27,22 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.mbNavigation.setOnClickListener {
+            val name = binding.etName.text.toString()
 
-
-            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment("emna"))
+            if (!name.isNullOrBlank()) {
+                findNavController().navigate(
+                    SplashFragmentDirections.actionSplashFragmentToHomeFragment(
+                        name
+                    )
+                )
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Veuillez saisir votre nom pour naviguer",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 }
+
