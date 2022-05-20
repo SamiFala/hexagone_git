4e10a75e6b1f2890986b76e4fa2aa997b6440149
waigenius <wailekone@gmail.com>
2022-05-20 15:57:21 +0200

    condition pour que la variable Count ne soit pas négative

--- app/src/main/java/com/wai/myapplication/HomeFragment.kt
+++ app/src/main/java/com/wai/myapplication/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -37,6 +38,12 @@ class HomeFragment: Fragment() {
 
         viewModel.count.observe(viewLifecycleOwner, Observer{
             binding.tvTitle.text = getString(R.string.bonjour_s_welcome_to_my_app, navArgs.name, viewModel.count.value.toString())
+        })
+
+        viewModel.error.observe(viewLifecycleOwner, Observer {
+            if (it){
+                Toast.makeText(requireContext(), "Une erreur est survenue", Toast.LENGTH_LONG).show()
+            }
         })
 
         binding.mbPlus.setOnClickListener(){
--- app/src/main/java/com/wai/myapplication/MainViewModel.kt
+++ app/src/main/java/com/wai/myapplication/MainViewModel.kt
@@ -7,17 +7,28 @@ class MainViewModel : ViewModel() {
 
     //Implémentation du Live Data :  pour observer l'évolution de la donnée
     var count: MutableLiveData<Int> = MutableLiveData(0)
+    var error: MutableLiveData<Boolean> = MutableLiveData(false)
 
     fun plus(){
         count.value = count.value?.plus(1)
     }
 
     fun moins(){
-        count.value = count.value?.minus(1)
+        count.value?.let {
+            if (it > 0 ){
+                count.value = count.value?.minus(1)
+                error.value = false
+            }else{
+                error.value = true
+            }
+        }
+
 
     }
 
     fun reset(){
         count.value = 0
     }
+
+
 }
