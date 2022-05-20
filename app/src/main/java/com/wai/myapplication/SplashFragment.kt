c0eb6123fd3a0c0537e8c6da62ffb55ae8a44622
waigenius <wailekone@gmail.com>
2022-05-20 10:25:26 +0200

    Validation et vérification du chamsps de saisie

--- app/src/main/java/com/wai/myapplication/SplashFragment.kt
+++ app/src/main/java/com/wai/myapplication/SplashFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.wai.myapplication.databinding.SplashLayoutBinding
@@ -29,7 +30,16 @@ class SplashFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.mbNavigation.setOnClickListener{
-            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment(name="Waï"))
+            // Récupère la valeur saisie après le click du bouton
+            val name = binding.etName.text.toString()
+
+            // Condition pour ne valider que un nom non null
+            if(!name.isNullOrBlank()){
+                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment(name))
+            }else{
+                Toast.makeText(requireContext(), "Veuillez entrer votre nom", Toast.LENGTH_LONG).show()
+            }
+
 
         }
 
